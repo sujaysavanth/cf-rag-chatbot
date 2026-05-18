4bac5737877ae9deb5a9553a9260a041ede8efd2
sujaysavanth <sgovin42@asu.edu>
2026-05-17 17:54:03 -0700

    Initial commit

--- src/entry.py
+++ src/entry.py
@@ -1,5 +1,18 @@
+from urllib.parse import urlparse
 from workers import Response, WorkerEntrypoint
 from submodule import get_hello_message
+
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        return Response(get_hello_message())
+        url = urlparse(request.url)
+        method = request.method
+
+        if url.path == "/hello" and method == "GET":
+            return Response(get_hello_message())
+        if url.path == "/ingest" and method =="POST":
+            return Response("Data ingested successfully", status=200)
+        
+        if url.path == "/query" and method == "GET":
+            return Response("Query executed successfully",status = 200)
+
+        return Response("Not found", status=404)
